f6eaa46306f0f68ba28f4b39397dccb16a9e2539
ysjeong1104 <whseok@icloud.com>
2023-01-18 21:53:40 +0900

    graphql update

--- src/app.js
+++ src/app.js
@@ -1,21 +1,45 @@
 import { ApolloServer,gql } from "apollo-server";
  
+const users=[{
+        id:"1",
+        firstName:"정",
+        lastName:"영석"
+    },
+    {
+        id:"2",
+        firstName:"Elton",
+        lastName:"Jhon"
+    }
+]
+let tweets=[
+    {
+        id:"1",
+        text:"first one"        
+    },
+    {
+        id:"2",
+        text:"second one"
+    }
+
+]
 
 const typeDefs = gql`
 type User {
     id: ID!
     username: String!
-    firstname: String!
-    lastname: String!
+    firstName: String!
+    lastName: String!
+    fullName: String!
 }
 type Tweet {
     id:ID!
     text:String!
-    author: User!
+    author: User
 }
 type Query {
-  allTwees: [Tweet!]!
+  allTweets: [Tweet!]!
   tweet(id: ID):Tweet
+  allUsers: [User!]!
 }
 type Mutation {
     postTweet(text:String!,userId: ID!): Tweet!
@@ -25,9 +49,39 @@ type Mutation {
 
 const resolvers={
     Query : {
-        tweet : ()=>{
-            console.log("im called");
-            return null;
+        allTweets : ()=>{
+            return tweets;
+        },
+        tweet : (_,{id})=>{
+            return tweets.find(tweet=>tweet.id === id)
+        },
+        allUsers: ()=>{
+            return users;
+        }
+    },
+    Mutation : {
+        postTweet : (_, {text,userId})=>{
+            const newTweet={
+                id : tweets.length+1,
+                text,
+            };
+            tweets.push(newTweet);
+
+            return newTweet;
+        },
+        deleteTweet:(_,{id})=>{
+
+            const tweet = tweets.find((tweet)=> tweet.id === id);
+            if(!tweet) return false;
+            tweets = tweets.filter(tweet=> tweet.id !== id);
+            return true
+        },
+    },
+    User:{
+        fullName:({firstName,lastName})=>{
+            console.log("fullName Called");
+            //console.log(root);
+            return firstName+' '+lastName;
         }
     }
 }
